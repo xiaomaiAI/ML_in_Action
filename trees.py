d413da3798093593c00df8a0280f9910c1a37800
xiaomaiAI <xiaomaiAI@live.cn>
2019-04-02 17:03:31 +0800

    2019-04-02

--- trees.py
+++ trees.py
@@ -64,19 +64,35 @@ def createTree(dataSet, labels):
 	classList = [example[-1] for example in dataSet]
 	if classList.count(classList[0]) == len(classList):
 		return classList[0]
-	if len(dataSet[0]) == 1:
+	#if all are same,stop
+	if len(dataSet[0]) == 1:#recursion to this
 		return majorityCnt(classList)
+	#return the major class
 	bestFeat = chooseBestFeatureToSplit(dataSet)
 	bestFeatLabel = labels[bestFeat]
-	myTree = {bestFeatLabel:{}}
+	myTree = {bestFeatLabel:{}}#recursion dic
 	del(labels[bestFeat])
 	featValues = [example[bestFeat] for example in dataSet]
 	uniqueVals = set(featValues)
+	#delete the redundance
 	for value in uniqueVals:
 		subLabels = labels[:]
 		myTree[bestFeatLabel][value] = createTree(splitDataSet(
 			dataSet, bestFeat, value), subLabels)
 	return myTree
+
+def classify(inputTree, featLabels, testVec):
+	firstStr = inputTree.keys()[0]
+	secondDict = inputTree[firstStr]
+	featIndex = featLabels.index(firstStr)
+	for key in secondDict.keys():
+		if testVec[featIndex] == key:
+			if type(secondDict[key]).__name__ == "dict":
+				classLabel =classify(secondDict[key], featLabels, testVec)
+			else:
+				classLabel = secondDict[key]
+	return classLabel
+	
 
 	
 
